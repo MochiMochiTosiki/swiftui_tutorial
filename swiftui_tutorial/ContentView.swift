067a879e043933168a9336336af956d06379df85
toshiki <mochi.mochi.0397@gmail.com>
2021-07-09 23:43:56 +0900

    Initial Commit

---
+++ swiftui_tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_tutorial
+//
+//  Created by 望月紀生 on 2021/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_tutorial/swiftui_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tutorialApp.swift
+//  swiftui_tutorial
+//
+//  Created by 望月紀生 on 2021/07/09.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
