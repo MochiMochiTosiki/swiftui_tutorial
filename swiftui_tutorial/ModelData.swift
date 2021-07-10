fc84cb1be19e8b28a01f39602143aae4d59fa13e
toshiki <mochi.mochi.0397@gmail.com>
2021-07-10 15:27:36 +0900

    section1(views, model, resources)

---
+++ swiftui_tutorial/ModelData.swift
@@ -0,0 +1,31 @@
+//
+//  ModelData.swift
+//  swiftui_tutorial
+//
+//  Created by 望月紀生 on 2021/07/10.
+//
+
+import Foundation
+
+var ladnmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
